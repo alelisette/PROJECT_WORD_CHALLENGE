cce4ebfbb8b9834c76efa1ee8e35d633fbe405bd
maricarmen5 <mcarmen1804@gmail.com>
2024-11-13 19:11:46 +0100

    implementació word_toolkit::es_canonic

--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -1,1 +1,13 @@
 #include "word_toolkit.hpp"
+
+bool word_toolkit::es_canonic(const string& s) throw() {
+    bool canonic = true;
+    int i = 1;
+
+    while (canonic and i < (int)s.size()) {
+        if (s[i] < s[i-1]) canonic = false;
+        ++i;
+    }
+
+    return canonic;
+}
