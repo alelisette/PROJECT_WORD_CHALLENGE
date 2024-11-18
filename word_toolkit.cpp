3f9d2bbfe2ca94a76d0b155fa5322b4cbcebdcdb
maricarmen5 <mcarmen1804@gmail.com>
2024-11-18 15:18:32 +0100

    Implementació word_toolkit::anagrama_canonic

--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -1,5 +1,7 @@
 #include "word_toolkit.hpp"
+#include <map>
 
+// Cost temporal: O(n)
 bool word_toolkit::es_canonic(const string& s) throw() {
     bool canonic = true;
     int i = 1;
@@ -12,6 +14,14 @@ bool word_toolkit::es_canonic(const string& s) throw() {
     return canonic;
 }
 
-string word_toolkit::anagrama_canonic(const string& s) throw() {
-    
+// Cost temporal: O(log(n))
+string word_toolkit::anagrama_canonic(const string& s) throw() { 
+    string canonic = s;
+    sort(canonic.begin(), canonic.end()); // ? profe
+    return canonic;
+}
+
+
+char mes_frequent(const string& excl, const list<string>& L) throw() {
+
 }
