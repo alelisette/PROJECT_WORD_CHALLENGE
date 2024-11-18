e6d0d02cb2ced01ea9e277511373eb238743cb3d
maricarmen5 <mcarmen1804@gmail.com>
2024-11-18 16:10:49 +0100

    Implementació word_toolkit::mes_frequent

--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -1,7 +1,6 @@
 #include "word_toolkit.hpp"
-#include <map>
 
-// Cost temporal: O(n)
+// Cost temporal: O(n), n = s.size()
 bool word_toolkit::es_canonic(const string& s) throw() {
     bool canonic = true;
     int i = 1;
@@ -14,14 +13,42 @@ bool word_toolkit::es_canonic(const string& s) throw() {
     return canonic;
 }
 
-// Cost temporal: O(log(n))
+// Cost temporal: O(n*log(n)), n = s.size()
 string word_toolkit::anagrama_canonic(const string& s) throw() { 
     string canonic = s;
-    sort(canonic.begin(), canonic.end()); // ? profe
+    sort(canonic.begin(), canonic.end());
     return canonic;
 }
 
+// Cost temporal: O(n^2), n = L.size()
+char word_toolkit::mes_frequent(const string& excl, const list<string>& L) throw() {
+    //cost constant  O(1)
+    bool excl_array[26] = {false};
+    for (int i = 0; i < excl.size(); ++i) {
+        excl_array[i-'A'] = true; 
+    }
 
-char mes_frequent(const string& excl, const list<string>& L) throw() {
+    int freq_excl_array[26] = {0};
+    iterador it = L.begin();
+    while (it != L.end()) {
+        string paraula = *it;
+        for (int i = 0; i < paraula.size(); ++i) {
+            char lletra = paraula[i];
+            if (not excl_array[i-'A']) freq_excl_array[i-'A']++;
+        }
+    }
 
+    int max_freq = 0;
+    char c = '\0';
+    for (int i = 0; i < freq_excl_array.size(); ++i) {
+        if (max_freq < freq_excl_array[i]) {
+            max_freq = freq_excl_array[i];
+            c = i + 'A';
+        } else if (max_freq == freq_excl_array[i]) {
+            char d = i + 'A';
+            if (d < c) c = d;
+        }
+    }
+
+    return c;
 }
