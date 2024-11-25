6f75d10c9d75283f57f766aca00523f47161d0e7
maricarmen5 <mcarmen1804@gmail.com>
2024-11-25 15:48:25 +0100

    iter_subset::iter_subset(nat n, nat k) implementat parcialment

--- iter_subset.cpp
+++ iter_subset.cpp
@@ -1,60 +1,72 @@
 #include "iter_subset.hpp"
 
-  /* Pre:  Cert
-     Post: Construeix un iterador sobre els subconjunts de k elements 
-     de {1, ..., n}; si k > n no hi ha res a recórrer. */
-  iter_subset::iter_subset(nat n, nat k) throw(error) {
+iter_subset::iter_subset(nat n, nat k) throw(error) {
+   if (n < k) throw runtime_error("Error: n < k");
 
-  }
+   int nombreCombinatori;
+   _mida = (nat)nombreCombinatori;
 
-  /* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
-  iter_subset::iter_subset(const iter_subset& its) throw(error) {
-    
-  }
-  iter_subset& iter_subset::operator=(const iter_subset& its) throw(error){
+   node* anterior = nullptr;
+   for (int i = 0; i < nombreCombinatori; ++i) {
+      node* nouNode = new node;
+      nouNode->info;
+      nouNode->seg = nullptr;
 
-  }
-  iter_subset::~iter_subset() throw() {
+      if (i == 0) _prim = nouNode;
+      else anterior->seg = nouNode;
+      anterior = nouNode;
+   }
+   _ult = anterior;
+}
 
-  }
+/* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
+iter_subset::iter_subset(const iter_subset& its) throw(error) {
+   
+}
+iter_subset& iter_subset::operator=(const iter_subset& its) throw(error){
 
-  /* Pre:  Cert
-     Post: Retorna cert si l'iterador ja ha visitat tots els subconjunts
-     de k elements presos d'entre n; o dit d'una altra forma, retorna
-     cert quan l'iterador apunta a un subconjunt sentinella fictici 
-     que queda a continuació de l'últim subconjunt vàlid. */
-  bool iter_subset::end() const throw() {
+}
+iter_subset::~iter_subset() throw() {
 
-  }
+}
 
-  /* Operador de desreferència.
-     Pre:  Cert 
-     Post: Retorna el subconjunt apuntat per l'iterador;
-     llança un error si l'iterador apunta al sentinella. */
-  subset iter_subset::operator*() const throw(error) {
+/* Pre:  Cert
+   Post: Retorna cert si l'iterador ja ha visitat tots els subconjunts
+   de k elements presos d'entre n; o dit d'una altra forma, retorna
+   cert quan l'iterador apunta a un subconjunt sentinella fictici 
+   que queda a continuació de l'últim subconjunt vàlid. */
+bool iter_subset::end() const throw() {
 
-  }       
+}
 
-  /* Operador de preincrement.
-     Pre:  Cert 
-     Post: Avança l'iterador al següent subconjunt en la seqüència i el retorna; 
-     no es produeix l'avançament si l'iterador ja apuntava al sentinella. */
-  iter_subset& iter_subset::operator++() throw() {
+/* Operador de desreferència.
+   Pre:  Cert 
+   Post: Retorna el subconjunt apuntat per l'iterador;
+   llança un error si l'iterador apunta al sentinella. */
+subset iter_subset::operator*() const throw(error) {
 
-  }
+}       
 
-  /* Operador de postincrement. 
-     Pre:  Cert
-     Post: Avança l'iterador al següent subconjunt en la seqüència i retorna el seu valor
-     previ; no es produeix l'avançament si l'iterador ja apuntava al sentinella. */
-  iter_subset iter_subset::operator++(int) throw() {
+/* Operador de preincrement.
+   Pre:  Cert 
+   Post: Avança l'iterador al següent subconjunt en la seqüència i el retorna; 
+   no es produeix l'avançament si l'iterador ja apuntava al sentinella. */
+iter_subset& iter_subset::operator++() throw() {
 
-  }
+}
 
-  /* Operadors relacionals. */
-  bool iter_subset::operator==(const iter_subset& c) const throw() {
+/* Operador de postincrement. 
+   Pre:  Cert
+   Post: Avança l'iterador al següent subconjunt en la seqüència i retorna el seu valor
+   previ; no es produeix l'avançament si l'iterador ja apuntava al sentinella. */
+iter_subset iter_subset::operator++(int) throw() {
 
-  } 
-  bool iter_subset::operator!=(const iter_subset& c) const throw() {
+}
 
-  }
+/* Operadors relacionals. */
+bool iter_subset::operator==(const iter_subset& c) const throw() {
+
+} 
+bool iter_subset::operator!=(const iter_subset& c) const throw() {
+
+}
