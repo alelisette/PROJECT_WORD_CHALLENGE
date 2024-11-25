f3e2434226835618e840e478939342f4a720ea8a
maricarmen5 <mcarmen1804@gmail.com>
2024-11-25 15:37:31 +0100

    JocsDeProva

--- iter_subset.cpp
+++ iter_subset.cpp
@@ -1,1 +1,20 @@
 #include "iter_subset.hpp"
+
+iter_subset::iter_subset(nat n, nat k) throw(error) {
+    if (n < k) throw runtime_error("Error: n < k");
+
+    int nombreCombinatori;
+    _mida = (nat)nombreCombinatori;
+
+    node* anterior = nullptr;
+    for (int i = 0; i < nombreCombinatori; ++i) {
+        node* nouNode = new node;
+        nouNode->info;
+        nouNode->seg = nullptr;
+
+        if (i == 0) _prim = nouNode;
+        else anterior->seg = nouNode;
+        anterior = nouNode;
+    }
+    _ult = anterior;
+}
